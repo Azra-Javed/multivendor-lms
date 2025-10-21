79a3c30d250b9d9c71a9d30ec5f292f8cb16f40a
Azra Javed <azraj063@gmail.com>
2025-10-21 22:28:17 +0500

    database, cloudinary and redis connection

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./utils/db.js";
 
 dotenv.config();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
 });
+
+// connect db
+connectDB();

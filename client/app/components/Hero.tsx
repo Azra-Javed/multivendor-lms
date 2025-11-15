af108e8ea681fb9423cddb2b1e65466c46195f44
Azra Javed <azraj063@gmail.com>
2025-11-15 14:31:54 +0500

    create hero component

--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BiSearch } from "react-icons/bi";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="w-full 1000px:flex items-center">
+      <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[40vh] left-5 w-[40vh] hero_animation rounded-[50%] 1100px:left-8 1500px:left-14"></div>
+      <div>
+        <Image
+          src={require("../../public/assets/banner.png")}
+          alt="banner"
+          width={400}
+          height={400}
+          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
+        />
+      </div>
+      <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
+        <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[50px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%]">
+          Improve Your Online Learning experience better instantly
+        </h2>
+        <br />
+        <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
+          we have 40K+ online courses & 500k+ online registered student. find yu
+          desired courses from them.
+        </p>
+        <br />
+        <br />
+        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
+          <input
+            type="text"
+            placeholder="Search Courses..."
+            className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
+          />
+          <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
+            <BiSearch className="text-white" size={30} />
+          </div>
+        </div>
+        <br />
+        <br />
+
+        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
+          <Image
+            src={require("../../public/assets/client-1.png")}
+            alt=""
+            className="rounded-full ml-[-20px]"
+          />
+          <Image
+            src={require("../../public/assets/client-2.png")}
+            alt=""
+            className="rounded-full ml-[-20px]"
+          />
+          <Image
+            src={require("../../public/assets/client-3.png")}
+            alt=""
+            className="rounded-full ml-[-20px]"
+          />
+          <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-semibold ">
+            500K+ people already trusted us.{" "}
+            <Link
+              href={"/courses"}
+              className="dark:text-[#46e256] text-[crimson]"
+            >
+              View Courses
+            </Link>
+          </p>
+        </div>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
